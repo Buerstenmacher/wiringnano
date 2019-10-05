daadd614baffe026b653e8e94d5d134346c40b16
Buerstenmacher <46110872+Buerstenmacher@users.noreply.github.com>
2019-10-05 12:09:05 +0200

    Add files via upload
    addition of comments

--- rom_wiringnano.h
+++ rom_wiringnano.h
@@ -63,23 +63,23 @@ namespace rom {	//contain everything in ::rom to avoid naming collision
 // **********************************************************************************
 // **********************************************************************************
 class file {			// create or open files; buffer them in ram; an use them if they where std::vector<uint8_t>
-private:                	// This Class is good handling Files that are smaler then 10MB
+private:                	// This class is good handling Files that are smaler then 10MB
 std::fstream 	fst;       	// It reads the whole file into ram and writes it out only
 std::string 	filename;   	// in case of change
-uint8_t 	is_unsync;      // 0 if sync, 1 if buffer is newer, 2 if file is newer
-std::vector<uint8_t> data;	// user data that should be writen to file before closing it
+uint8_t 	is_unsync;      // private status flag: 0 if sync, 1 if buffer is newer, 2 if file is newer
+std::vector<uint8_t> data;	// user data that should contain the entire file contents
 
 void open(const std::string& mode = "in") {
-fst.open(filename.c_str(),std::ios::out|std::ios::app); //This should create the file if it does not exist
-fst.close();
-if (mode == "out")      {fst.open(filename.c_str(),std::ios::out);}	//open for writing
-if (mode == "in")       {fst.open(filename.c_str(),std::ios::in);}	//open for reading
+fst.open(filename.c_str(),std::ios::out|std::ios::app); 	//This should create the file if it does not exist
+fst.close();							//close the file
+if (mode == "out")      {fst.open(filename.c_str(),std::ios::out);}	//open it again for writing
+if (mode == "in")       {fst.open(filename.c_str(),std::ios::in);}	//open it again for reading
 if (!fst.is_open()) {throw std::runtime_error("A file with name "+filename+" failed to open in Mode: "+mode);}
 }
 
-void close(void)        {fst.close();}
+void close(void)        {fst.close();}		//close file
 
-void flush(void) {
+void flush(void) {			//synchronise data with file
 if 	(!is_unsync) 	{return;}	//flush is fast if data is already syncronized
 else if (is_unsync == 1) {      	//write buffer to file
         open("out");			//open file for writing
@@ -89,7 +89,7 @@ else if (is_unsync == 1) {      	//write buffer to file
 else if (is_unsync == 2) {     		//read from file to buffer
 	open("in");			//open file for reading
         fst.seekg (0, std::ios::end);	//go to end of file
-        auto  size{fst.tellg()};  	//get size of file
+        auto size{fst.tellg()};  	//get size of file
 	data.resize(size);		//reserve enough memory in std::vector<uint8_t> data
         fst.seekg (0, std::ios::beg);	//go to begin of file
 	//the next line is certainly not what the inventors of c++ want us to do, :-P
@@ -102,24 +102,26 @@ close();			//close file, we do not keep it open longer than necessary
 }
 
 public:
-void reread(void) {
+void reread(void) {	//get data from file and discard values in local buffer
 is_unsync = 2;
 flush();
 }
 
-void rewrite(void) {
+void rewrite(void) {	//save local data to file
 is_unsync = 1;
 flush();
 }
 
+void save(void) {rewrite();}	//save local data to file
+
 //there is no construction without reading the content of file to data container
-//except if we are dealig with writeonly files from linux
+//except if we are dealig with strange writeonly files from linux; yes it's true, files can be "writeonly"
 file(const std::string& name = "time.log",uint8_t writeonly = 0):fst{},
 	filename(name),is_unsync(writeonly?1:2),data{} {	//defaultname is "time.log"
 flush();
 }
 
-~file(void) {flush();}	//file will be saved by destructor, there is NO "closing without saving changes"
+~file(void) {flush();}	//file will be saved by destructor,at this time there is NO "closing without saving changes"
 
 void push_back(uint8_t datain) {	//add one byte at the end of file
 data.push_back(datain);
@@ -134,8 +136,8 @@ void push_back(const std::string& stin) {for (auto ch:stin) {push_back(uint8_t(c
 
 uint8_t& at(size_t pos) {	//get reference to one byte of the file at position pos
 is_unsync = 1;
-if (pos>=data.size())  {data.resize(pos+1);}
-return data.at(pos);
+if (pos>=data.size())  {data.resize(pos+1);}	//increase size of data if pos is larger than current size of file
+return data.at(pos);				//it may create some garbage data in the file!!!!
 }
 
 uint8_t at(size_t pos) const {	//get a copy of the one byte at pos
@@ -155,19 +157,18 @@ is_unsync = 1;
 
 // **********************************************************************************
 // **********************************************************************************
-class wiringnano {// this class creates a rpresentation for one GPIO Pin on Jeton Nano
+class wiringnano {// this class creates a representation for one GPIO Pin on Jeton Nano
 private:
 uint8_t wp;		//pinnumber for gpio;
 uint8_t val;		//latest output value: 0->low  1->high  3->input
-rom::file* exp;  	//the files are pointers to get the best control of construction an destruction timing
+rom::file* exp;  	//the files are pointers to rom::file to get the best control of construction an destruction timing
 rom::file* unexp;	//files for export and unexport gpios to sysf interface
 rom::file* dirfile;	//file for controlling it's dirrection: output? input?
 rom::file* valuefile;	//file for gpio value: 0 or 1
 
-
-static const std::vector<uint8_t>& available_gpio(void) {
-static std::vector<uint8_t> available_gpio_int{216,50,79,14,194,232,15,16,17,13,18,
-						19,20,149,200,168,38,76,51,12,77,78};
+static const std::vector<uint8_t>& available_gpio(void) {//in class initialisation of static variables is strange in c++11 because
+static std::vector<uint8_t> available_gpio_int{216,50,79,14,194,232,15,16,17,13,18,	//we have to wrap it inside a static member function
+						19,20,149,200,168,38,76,51,12,77,78};	//and return it by const reference
 return available_gpio_int;
 }
 
@@ -186,24 +187,24 @@ static std::string unexport_path_int{"/sys/class/gpio/unexport"};
 return unexport_path_int;
 }
 
-std::string pinstr(void) 		{return std::to_string(wp);}
-std::string direction_path(void) 	{return std::string{"/sys/class/gpio/gpio"+pinstr()+"/direction"};}
-std::string value_path(void) 		{return std::string{"/sys/class/gpio/gpio"+pinstr()+"/value"};}
+std::string pinstr(void) 		{return std::to_string(wp);}	//get pinnumber as human readable std::string
+std::string direction_path(void) 	{return std::string{"/sys/class/gpio/gpio"+pinstr()+"/direction"};}	//create a std::string that contains the entire path of file
+std::string value_path(void) 		{return std::string{"/sys/class/gpio/gpio"+pinstr()+"/value"};}		//create a std::string that contains the entire path of file
 
-void export_(void) {
+void export_(void) {	//echo ?? > /sys/class/gpio/export
 exp->resize(0);
 exp->push_back(pinstr());
 exp->rewrite();
 }
 
-void unexport_(void) {
+void unexport_(void) {	//echo ?? > /sys/class/gpio/unexport
 unexp->resize(0);
 unexp->push_back(pinstr());
 unexp->rewrite();
 }
 
-void set_direction(const std::string& dir) {
-for (auto& s:available_directions()) {
+void set_direction(const std::string& dir) {	//echo out > /sys/class/gpio/gpio??/direction
+for (auto& s:available_directions()) {		//echo in > /sys/class/gpio/gpio??/direction
 	if (dir == s)	{
 		dirfile->resize(0);
 		dirfile->push_back(dir);
@@ -214,15 +215,16 @@ for (auto& s:available_directions()) {
 throw std::runtime_error("Direction: "+dir+" is not available for gpio"+pinstr()+".");
 }
 
-uint8_t read_value(void) {
-valuefile->reread();	//reread from file on disk
+uint8_t read_value(void) {	//return value of file:  /sys/class/gpio/gpio79/value
+valuefile->reread();		//reread from file on disk
 return (valuefile->at(0)=='0')?0:1;
 }
 
 public:
-wiringnano(const wiringnano&) = delete;
-wiringnano operator=(const wiringnano&) = delete;
+wiringnano(const wiringnano&) = delete;			//no copy until now
+wiringnano operator=(const wiringnano&) = delete;	//no copy until now
 
+//for construction you have to provide gpio number; see table at the begin of this file
 wiringnano(uint8_t nr):wp(nr),val{3},exp{nullptr},unexp{nullptr},dirfile{nullptr},valuefile{nullptr} {
 if (std::find(available_gpio().begin(),available_gpio().end(),nr)==available_gpio().end())
 	{throw std::runtime_error("Gpio: "+pinstr()+" is not available.");}
@@ -234,7 +236,7 @@ valuefile = new rom::file(value_path());
 set_direction("in");
 }
 
-~wiringnano(void) {
+~wiringnano(void) {	//destruction will be automatic, files will be closed in the right order
 set_direction("in");
 delete valuefile;
 delete dirfile;
@@ -243,29 +245,29 @@ delete unexp;
 delete exp;
 }
 
-void pullhi() {//set digital output to high voltage
+void pullhi() {	//set digital output to high voltage
 if (val != 1)	{set_direction("high");}
 val = 1;
 }
 
-void pulllo() {//set digital output to high voltage
+void pulllo() {	//set digital output to high voltage
 if (val != 0)	{set_direction("low");}
 val = 0;
 }
 
-void flow() {
+void flow() {	//set gpio pin to input mode without reading it's voltage level
 if (val !=3) {set_direction("in");}
 val =3;
 }
 
-void write(uint8_t bit) {
+void write(uint8_t bit) {	//write 0 or 1 to gpio pin
 if (val != bit) {
 	if (bit == 1) 		{pullhi();}
 	else if (bit == 0)	{pulllo();}
 }
 }
 
-uint8_t read() {
+uint8_t read() {	//read voltage level from gpio pin
 flow();
 return read_value();
 }
@@ -277,24 +279,23 @@ return read_value();
 
 //usage example function
 void wiringnano_t(void) {	//simple demo of gpio output on jetson nano
-//::rom::autodelay delay{};	//create a delay object from rom_time.h
 wiringnano pin79{79};		//create a wiringnano gpio interface for gpio 79 == pin 12
 wiringnano pin232{232};		//create a wiringnano gpio interface for gpio 232 == pin 16
 				// (see table at begin of file)
-std::cout <<  "Connect a Led to pin 12 of your Jetson-Nano and wire it via a 4k7 resistor to"<< std::endl;
+std::cout <<  "Connect a Led to pin 12 of your Jetson-Nano and wire it via a 4k7 resistor to"	<< std::endl;
 std::cout <<  "pin 6 (GND). Dim the lights of your room! It should be blinking for 20 seconds. "<< std::endl;
 for (uint32_t i{0};i<20;++i){						//loop 10 times
 	pin79.pulllo();							//send 0.0 Volt to pin 12
-	std::this_thread::sleep_for(std::chrono::milliseconds(500));	//wait 0.5 sec.
+	std::this_thread::sleep_for(std::chrono::milliseconds(250));	//wait 0.25 sec.
 	pin79.pullhi();							//send 3.3 Volt to pin 12
-	std::this_thread::sleep_for(std::chrono::milliseconds(500));	//wait 0.5 sec.
+	std::this_thread::sleep_for(std::chrono::milliseconds(250));	//wait 0.25 sec.
 	}
-std::cout <<  "We are stopping blinking now and measue the voltage on pin 16 "<< std::endl;
+std::cout << "We are stopping blinking now and measue the voltage on pin 16 "<< std::endl;
 std::cout << "Pin 16 is: " << (pin232.read()?"high":"low ") << std::endl; //read and output status of pin 16
-std::cout <<  "You can pull it high by connecting it via a 1k resistor to pin 2. "<< std::endl;
-std::cout <<  "You can pull it low by connecting it via a 1k resistor to pin 6. "<< std::endl;
-std::cout <<  "Do not make any direct connections without resistors!!! "<< std::endl;
-std::cout <<  "This will probably cause some damege to your Jetson-Nano.  :-( "<< std::endl;
+std::cout << "You can pull it high by connecting it via a 1k resistor to pin 1 (3.3V). "<< std::endl;
+std::cout << "You can pull it low by connecting it via a 1k resistor to pin 6 (GND). "<< std::endl;
+std::cout << "Do not make any direct connections without resistors!!! "<< std::endl;
+std::cout << "This will probably cause some damege to your Jetson-Nano.  :-( "<< std::endl;
 }
 
 }	//namespace rom
